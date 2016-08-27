f62b205b589656fd970c4513dac469bb0ec2082f
Karl Kula <store.karl.kula@gmail.com>
2016-08-27 15:27:38 +0200

    Refactor and make more useful
    Added buttons to
    Run reference and ANN solution and draw the result.
    Create and acumulate training data
    Run training "session"

--- src/main.c
+++ src/main.c
@@ -37,6 +37,7 @@ typedef struct
     gboolean visited; // allready processed by pathf. algo.
     double cost; // The minimum cost of reaching the goal from this node
     gboolean chosen; // True menas this node is part of the path.
+    gboolean chosenByAnn;
     // The route chosen by the reference path finding algorithm
     int route_x; // x-componenet of the step to the next node
     int route_y; // y-component of the step to the next node
@@ -76,6 +77,24 @@ static void configureAnn(struct fann *annToConfigure)
     fann_set_training_algorithm(annToConfigure, FANN_TRAIN_RPROP);
 }
 
+static void randomizeWavFrequencies()
+{
+    // Randomize frequency of waves that build up the terrain
+    for (int i = 0; i < NR_OF_WAVES; i++)
+    {
+        char frequency[10];
+        snprintf(frequency, 6, "%4lf", ((double)rand())/(RAND_MAX*4.0));
+        waveEntries[i].frequencyBuffer = gtk_entry_buffer_new(frequency, 4);
+        waveEntries[i].frequencyEntry = gtk_entry_new_with_buffer(waveEntries[i].frequencyBuffer);
+    }
+}
+
+static void directionToXY(int dir, int *x, int *y)
+{
+    int sign = 1 - (dir & 2);
+    *x = ((~dir) & 1) * sign;
+    *y = ((dir) & 1) * sign;
+}
 
 static double calcCost(int parentX, int parentY, int parentRX, int parentRY, double parentIntensity)
 {
@@ -91,9 +110,9 @@ static double calcCost(int parentX, int parentY, int parentRX, int parentRY, dou
         n->cost = DBL_MAX/2;
         for (int dir = 0; dir < 4; dir++)
         {
-            int sign = 1 - (dir & 2);
-            int r_x = ((~dir) & 1) * sign;
-            int r_y = ((dir) & 1) * sign;
+            int r_x;
+            int r_y;
+            directionToXY(dir, &r_x, &r_y);
 
             if ((r_x != -parentRX || r_y != -parentRY) &&
                 (thisX + r_x >= 0) &&
@@ -116,8 +135,61 @@ static double calcCost(int parentX, int parentY, int parentRX, int parentRY, dou
     return n->cost + COST_OF_ONE_STEP + fmax(parentIntensity - n->intensity, 0);
 }
 
+static bool insideAndNotChosenByAnn(const int x, const int y)
+{
+    if ((x < SIZE) &&
+        (y < SIZE) &&
+        (x >= 0) &&
+        (x >= 0) &&
+        (theData[x][y].chosenByAnn == FALSE))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+static void findNextCoord(fann_type *fann_input, int *x, int *y)
+{
+    fann_run(ann, fann_input);
+    int direction = -1;
+    fann_type value = ann->output[0];
+    for (int i = 1; i < 4; i++)
+    {
+        if (ann->output[i] > value) direction = i;
+    }
+
+    directionToXY(direction, x, y);
+}
+
+static void intensityToInput(fann_type *input)
+{
+    for (int i = 0; i < SIZE * SIZE; i++)
+    {
+        input[i] = theData[i/SIZE][i%SIZE].intensity;
+    }
+}
+
+static void runAnn()
+{
+    fann_type fann_input[ANN_NUM_INPUT];
+    intensityToInput(fann_input);
+    int x, y = SIZE - 1;
+
+    while (insideAndNotChosenByAnn(x,y))
+    {
+        theData[x][y].chosenByAnn = TRUE;
+        fann_input[SIZE * SIZE] = (fann_type)x;
+        fann_input[SIZE * SIZE + 1] = (fann_type)y;
+        findNextCoord(fann_input, &x,&y);
+    }
+}
+
 static void prepareData()
 {
+    printf("Preparing the data\n");
 #define START_INTENSITY 0.5
     maxIntensity = START_INTENSITY;
     for (int w = 0; w < NR_OF_WAVES; w++)
@@ -145,11 +217,6 @@ static void prepareData()
 }
 
 
-static void hello(GtkWidget *widget,
-                  gpointer data )
-{
-    g_print("Hello World\n");
-}
 
 static gboolean delete_event(GtkWidget *widget,
                              GdkEvent *event,
@@ -181,18 +248,27 @@ static gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data)
     {
         for(int j = 0; j < SIZE; j++)
         {
+            // Paint landscape or the paths chosen.
             double intensity = theData[i][j].intensity / maxIntensity;
-            if (theData[i][j].chosen)
-        {
-            cairo_set_source_rgb(cr, 1, 0, 0);
-        }
-        else
-        {
-        cairo_set_source_rgb(cr,
-                             intensity,
-                             intensity,
-                             intensity);
-        }
+            if (theData[i][j].chosen && theData[i][j].chosenByAnn)
+            {
+                cairo_set_source_rgb(cr, 1, 1, 0);
+            }
+            else if (theData[i][j].chosen)
+            {
+                cairo_set_source_rgb(cr, 1, 0, 0);
+            }
+            else if (theData[i][j].chosenByAnn)
+            {
+                cairo_set_source_rgb(cr, 0, 1, 0);
+            }
+            else
+            {
+                cairo_set_source_rgb(cr,
+                                     intensity,
+                                     intensity,
+                                     intensity);
+            }
             cairo_rectangle(cr,
                             i*ELEMENT_SIZE,
                             j*ELEMENT_SIZE,
@@ -213,86 +289,180 @@ static gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data)
 }
 
 
-static void calculateAndDraw(GtkWidget *widget,
-                             gpointer data)
+// For one solution recorded in "theData" - extract data for future ANN training;
+static void extractTrainingData()
 {
-    GtkWidget *drawing_area = (GtkWidget*)data;
+    // Go throught the selected nodes and add trainingdata
+    int x = SIZE - 1;
+    int y = x;
+    dataNode * n = &theData[x][y];
 
-    // Generate the data
-    printf("Preparing the data\n");
-    prepareData();
+    struct fann_train_data *tmpTrainingData = fann_create_train(1, ANN_NUM_INPUT, ANN_NUM_OUTPUT);
 
+    while(n->route_x != 0 || n->route_y != 0)
+    {
+        intensityToInput(tmpTrainingData->input[0]);
+        tmpTrainingData->input[0][SIZE * SIZE] = x;
+        tmpTrainingData->input[0][SIZE * SIZE + 1] = y;
+        tmpTrainingData->output[0][0] = -1.0;
+        tmpTrainingData->output[0][1] = -1.0;
+        tmpTrainingData->output[0][2] = -1.0;
+        tmpTrainingData->output[0][3] = -1.0;
+        tmpTrainingData->output[0][n->dir] = 1.0;
+
+        // Merge the training data with existing set.
+        trainingData = fann_merge_train_data(trainingData, tmpTrainingData);
+
+        x += n->route_x;
+        y += n->route_y;
+        n = &theData[x][y];
+    }
+
+    fann_destroy_train(tmpTrainingData);
+}
+
+static void findPath()
+{
     // Find path
     printf("The cost from SIZE, SIZE to 0,0 is %lf\n", calcCost(SIZE-1 ,SIZE-1,0,0,theData[SIZE-1][SIZE-1].intensity));
 
-    // Prepare ANN tranining Data
-    // Create temporary training data set
-    struct fann_train_data *tmpTrainingData = fann_create_train(1, ANN_NUM_INPUT, ANN_NUM_OUTPUT);
-
-    for (int i = 0; i < SIZE * SIZE; i++)
-    {
-        tmpTrainingData->input[0][i] = theData[i/SIZE][i%SIZE].intensity;
-    }
-
     // Go throught the selected nodes and mark them.
-    // At the same time, add trainingdata
     int x = SIZE - 1;
     int y = x;
     dataNode * n = &theData[x][y];
 
     while(n->route_x != 0 || n->route_y != 0)
     {
-
-
-
-        if (EXTEND_TRAINING_DATA)
-        {
-            for (int i = 0; i < SIZE * SIZE; i++)
-            {
-                tmpTrainingData->input[0][i] = theData[i/SIZE][i%SIZE].intensity;
-            }
-            tmpTrainingData->input[0][SIZE * SIZE] = x;
-            tmpTrainingData->input[0][SIZE * SIZE + 1] = y;
-            tmpTrainingData->output[0][0] = -1.0;
-            tmpTrainingData->output[0][1] = -1.0;
-            tmpTrainingData->output[0][2] = -1.0;
-            tmpTrainingData->output[0][3] = -1.0;
-            tmpTrainingData->output[0][n->dir] = 1.0;
-
-            // Merge the training data with existing set.
-            trainingData = fann_merge_train_data(trainingData, tmpTrainingData);
-
-/*
-            if (trainingEnabled)
-            {
-                fann_train_on_data(ann, trainingData, max_epochs, epochs_between_reports, desired_error);
-            }
-            else
-            {
-                fann_type *annResults;
-                annResults = fann_run(ann, trainingData->input[0]);
-            }
-*/
-        }
         n->chosen = TRUE;
         x += n->route_x;
         y += n->route_y;
         n = &theData[x][y];
     }
+}
+
+
+static void tryAlgorithms(GtkWidget *widget,
+                          gpointer data)
+{
+    GtkWidget *drawing_area = (GtkWidget*)data;
+
+    // Generate the data
+    prepareData();
+
+    // Run the reference algorithm
+    findPath();
+
+    // Run the Neural Network
+    runAnn();
 
     // Redraw
     gtk_widget_queue_draw(drawing_area);
 }
 
-
-int main(int argc, char *argv[])
+static void gatherTrainingData()
 {
+    // Generate the data
+    prepareData();
+
+    // Run the reference algorithm
+    findPath();
+
+    // Extract training data from the results
+    extractTrainingData();
+}
+
+static void trainAnn()
+{
+    #define MAX_EPOCHS 25
+    #define EPOCHS_BETWEEN_REPORTS 5
+    #define DESIRED_ERROR 0.1
+
+    fann_train_on_data(ann,
+                       trainingData,
+                       MAX_EPOCHS,
+                       EPOCHS_BETWEEN_REPORTS,
+                       DESIRED_ERROR);
+}
+
+
+static void startGUI(int argc, char *argv[])
+{
+
     GtkWidget *window;
-    GtkWidget *button;
+    // Buttons:
+    GtkWidget *runAnnButton;
+    GtkWidget *gatherDataButton;
+    GtkWidget *trainButton;
     GtkWidget *closeButton;
+
     GtkWidget *drawing_area;
     GtkWidget *box;
     GtkWidget *drawingBox;
+
+    gtk_init(&argc, &argv);
+
+    /* create a new window */
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    g_signal_connect(window, "delete-event",
+                      G_CALLBACK(delete_event), NULL);
+    g_signal_connect(window, "destroy",
+                     G_CALLBACK(destroy), NULL);
+
+    // Create the drawing area on which we will paint terrain and paths.
+    drawing_area = gtk_drawing_area_new();
+    gtk_widget_set_size_request(drawing_area, SIZE*ELEMENT_SIZE, SIZE*ELEMENT_SIZE);
+    gtk_widget_add_events(drawing_area,
+                          GDK_BUTTON_PRESS_MASK);
+    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(draw_callback), NULL);
+
+    // Create a box (container) for the buttons and fields.
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    // Create a box to contain the drawin area
+    drawingBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+
+    runAnnButton = gtk_button_new_with_label("Run Neural Net");
+    gatherDataButton = gtk_button_new_with_label("Gather training data");
+    trainButton = gtk_button_new_with_label("Train Neural Net");
+    closeButton = gtk_button_new_with_label("Close");
+
+    // Assign tasks for the buttons
+    g_signal_connect(runAnnButton, "clicked",
+              G_CALLBACK(tryAlgorithms), drawing_area);
+
+    g_signal_connect(gatherDataButton, "clicked",
+              G_CALLBACK(gatherTrainingData), NULL);
+
+    g_signal_connect(trainButton, "clicked",
+              G_CALLBACK(trainAnn), NULL);
+
+    g_signal_connect(closeButton, "clicked",
+              G_CALLBACK(destroy), NULL);
+
+
+    // Pack the buttons and field into the box. top to bottom:
+    gtk_box_pack_start(GTK_BOX(box), runAnnButton, FALSE, FALSE, 3);
+    gtk_box_pack_start(GTK_BOX(box), gatherDataButton, FALSE, FALSE, 3);
+    for(int i = 0; i < NR_OF_WAVES; i++)
+    {
+        gtk_box_pack_start(GTK_BOX(box), waveEntries[i].frequencyEntry, FALSE, FALSE, 3);
+    }
+    gtk_box_pack_start(GTK_BOX(box), trainButton, FALSE, FALSE, 3);
+    gtk_box_pack_start(GTK_BOX(box), closeButton, FALSE, FALSE, 3);
+
+    /* This packs the  box and drawingBox into the window (a gtk container). */
+    gtk_box_pack_start(GTK_BOX(drawingBox), box, FALSE, FALSE, 3);
+    gtk_box_pack_start(GTK_BOX(drawingBox), drawing_area, FALSE, FALSE, 3);
+    gtk_container_add(GTK_CONTAINER (window), drawingBox);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+}
+
+
+int main(int argc, char *argv[])
+{
+
 
     FILE *trainingFile;
     snprintf(trainDataFilename, NAME_LENGTH, TRAIN_DATA_FILENAME, ANN_NUM_INPUT, ANN_NUM_OUTPUT);
@@ -328,58 +498,7 @@ int main(int argc, char *argv[])
                                          ANN_NUM_OUTPUT); // Lets start with an empty data set and then merge a new one for each trial. Perhaps slow but lets see.
     }
 
-
-
-    gtk_init(&argc, &argv);
-
-    /* create a new window */
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-    g_signal_connect(window, "delete-event",
-                      G_CALLBACK(delete_event), NULL);
-    g_signal_connect(window, "destroy",
-                     G_CALLBACK(destroy), NULL);
-
-    drawing_area = gtk_drawing_area_new();
-    gtk_widget_set_size_request(drawing_area, SIZE*ELEMENT_SIZE, SIZE*ELEMENT_SIZE);
-    gtk_widget_add_events(drawing_area,
-                          GDK_BUTTON_PRESS_MASK);
-    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(draw_callback), NULL);
-
-    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    drawingBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-
-    button = gtk_button_new_with_label("Generate\nand\ncalculate");
-    closeButton = gtk_button_new_with_label("Close");
-
-    // Randomize frequency of waves
-    for (int i = 0; i < NR_OF_WAVES; i++)
-    {
-        char frequency[10];
-        snprintf(frequency, 6, "%4lf", ((double)rand())/(RAND_MAX*4.0));
-        waveEntries[i].frequencyBuffer = gtk_entry_buffer_new(frequency, 4);
-        waveEntries[i].frequencyEntry = gtk_entry_new_with_buffer(waveEntries[i].frequencyBuffer);
-    }
-
-    g_signal_connect(button, "clicked",
-              G_CALLBACK(calculateAndDraw), drawing_area);
-
-    g_signal_connect(closeButton, "clicked",
-              G_CALLBACK(destroy), NULL);
-
-    /* This packs the button into the window (a gtk container). */
-    gtk_container_add(GTK_CONTAINER (window), drawingBox);
-    gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 3);
-    for(int i = 0; i < NR_OF_WAVES; i++)
-    {
-        gtk_box_pack_start(GTK_BOX(box), waveEntries[i].frequencyEntry, FALSE, FALSE, 3);
-    }
-    gtk_box_pack_start(GTK_BOX(box), closeButton, FALSE, FALSE, 3);
-    gtk_box_pack_start(GTK_BOX(drawingBox), box, FALSE, FALSE, 3);
-    gtk_box_pack_start(GTK_BOX(drawingBox), drawing_area, FALSE, FALSE, 3);
-
-    gtk_widget_show_all(window);
-    gtk_main();
+    startGUI(argc, argv);
 
     // Save and destroy training data
     fann_save_train(trainingData, trainDataFilename);
